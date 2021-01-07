0d68cbe7c8f1cdae14f82fcb4aae4964e917b6f2
john00neill <john2984@live.com>
2021-01-07 21:32:14 +0000

    feat: setTimeout() function added to main.js

--- main.js
+++ main.js
@@ -6,18 +6,17 @@ xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 // Send Request
 xhr.send();
 
-function setData(jsonData) {
-    data = jsonData;
-}
-
 // Listener here is waiting for xhr's state to change, will add responseText if everything okay
 // xhr maintains an internal state as it completes various parts of our request operation
 // readystate of 4 means the operation has been completed
 // status code 200 means request succeeded
 xhr.onreadystatechange = function() {
-    console.log(this.readyState);
     if(this.readyState == 4 && this.status == 200) {
-        data = this.responseText;
-        setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+// setTimeOut() function takes 2 parameters(Callback function(), timeout in miliseconds)
+setTimeout(function() {
+    console.log(data);
+}, 500);

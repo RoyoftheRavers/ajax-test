ed6c48a2c9760fd52e0cd65662c58931248eea1a
john00neill <john2984@live.com>
2021-01-07 21:50:50 +0000

    feat: getData function added to main2.js

---
+++ main2.js
@@ -0,0 +1,20 @@
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+    // Open a Connection (GET to retrieve/POST to send, URL we want to retrieve/send to)
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    // Send Request
+    xhr.send(); 
+    // Listener here is waiting for xhr's state to change, will add responseText if everything okay
+    // xhr maintains an internal state as it completes various parts of our request operation
+    // readystate of 4 means the operation has been completed
+    // status code 200 means request succeeded
+    xhr.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+getData(function(data) {
+    console.log(data);
+})

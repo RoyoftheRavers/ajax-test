46c0c20f62cbe0de07ef648c1de4b09b2798e1a9
john00neill <john2984@live.com>
2021-01-07 19:20:01 +0000

    State and Status comments added to main.js

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ xhr.send();
 // Listener here is waiting for xhr's state to change, will add responseText if everything okay
 // xhr maintains an internal state as it completes various parts of our request operation
 // readystate of 4 means the operation has been completed
+// status code 200 means request succeeded
 xhr.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         document.getElementById("data").innerHTML = this.responseText;
     }
 };
-

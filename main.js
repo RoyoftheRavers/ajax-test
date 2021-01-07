c81f5e3e53487a0642506cff3dc98ca40d270f5f
john00neill <john2984@live.com>
2021-01-07 21:24:07 +0000

    feat: onreadstatechange() function updated in main.js

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 // Open a Connection (GET to retrieve/POST to send, URL we want to retrieve/send to)
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-// Send Function
+// Send Request
 xhr.send();
+
+function setData(jsonData) {
+    data = jsonData;
+}
 
 // Listener here is waiting for xhr's state to change, will add responseText if everything okay
 // xhr maintains an internal state as it completes various parts of our request operation
 // readystate of 4 means the operation has been completed
 // status code 200 means request succeeded
 xhr.onreadystatechange = function() {
+    console.log(this.readyState);
     if(this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        data = this.responseText;
+        setData(JSON.parse(this.responseText));
     }
 };
